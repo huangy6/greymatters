bd0ddb912ff92bea618558dc801d810723c39eac
huangy6 <huangy6@yahoo.com>
2016-06-05 16:26:24 -0400

    Migration to Mapbox.js

--- map.js
+++ map.js
@@ -1,8 +1,3 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiaHVhbmd5IiwiYSI6ImNpcDBrb241NTAyaWJ1MG00cjc1Nmcwb3UifQ.1q77Pdqwy-hMPyRDdJlfuA';
+L.mapbox.accessToken = 'pk.eyJ1IjoiaHVhbmd5IiwiYSI6ImNpcDBrb241NTAyaWJ1MG00cjc1Nmcwb3UifQ.1q77Pdqwy-hMPyRDdJlfuA';
 
-var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/light-v9', //stylesheet location
-    center: [0, 0], // starting position
-    zoom: 3 // starting zoom
-});
+var map = L.mapbox.map('map', 'mapbox.light').setView([0, 0], 3);
